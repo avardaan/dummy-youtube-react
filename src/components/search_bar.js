273c60faedc10a12682e8eda1ccc1f6a976f233c
Vardaan <vardaan119@gmail.com>
2018-02-26 02:30:31 -0500

    Manipulating state, working html input

---
+++ src/components/search_bar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+// define new class and give it all the functionalty of React.Component
+class SearchBar extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      term: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+      </div>
+    )
+  }
+
+  onInputChange(event) {
+    // when print event object, most values are null because of "event pooling"
+    // something to do with asynchronous nature and performance optimization that I don't understand
+    // event.target.value is the text inside input
+    this.setState({ term: event.target.value })
+  }
+
+}
+
+export default SearchBar
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
+// google api key for youtube
+const API_KEY = 'AIzaSyDDk7JB7WiWyx-0-E5xeqOiWXKzr8hswIQ'
+
 // component/function that returns JSX which is then transpiled to JS/HTML understood by the browser
 const App = () => {
-  return <div>
-    Hi
-  </div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 // render function throws first argument i.e. App component into a container, i.e. the div container in index.html
